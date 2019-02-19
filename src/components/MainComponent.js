b266480e6c7e23c041d95bdcc9848a0258ca680e
Kelsey Lewis <kelseygueldalewis@gmail.com>
2019-02-18 18:31:43 -0800

    Loading

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -22,7 +22,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes())}
 
 });
 
@@ -35,32 +36,37 @@ class Main extends Component {
 
     };
 
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render() {
+
     const HomePage = () => {
-      return (
-        <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-          promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]} />
+      return(
+          <Home 
+              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+              dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
+              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
-
     //because <Route link that calls this function has path="/menu/:dishId", 
     //everything after colon is passed as the {match} variable to the function.
     //In this case match = dishId as a string. Ex '0' - have to use match.params.name to access match value
     const DishWithId = ({match}) => {
       return(
-        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-        addComment={this.props.addComment}
-      />
-        // <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-        //   //returns a comments array
-        //   comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
-        //   //allows addComment function to be used
-        //   addComment={this.props.addComment}
-        //   />
+          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+            isLoading={this.props.dishes.isLoading}
+            errMess={this.props.dishes.errMess}
+            //   //returns a comments array
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            //   //allows addComment function to be used
+            addComment={this.props.addComment}
+          />
       );
-
     };
 
     return (
