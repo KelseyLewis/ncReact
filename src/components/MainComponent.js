95b4395627d5acbf60cad486231ffecb6924c642
Kelsey Lewis <kelseygueldalewis@gmail.com>
2019-02-18 23:31:49 -0800

    Fetch from server

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,8 +8,9 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+
 
 
 
@@ -18,16 +19,17 @@ const mapStateToProps = state => {
     dishes: state.dishes,
     comments: state.comments,
     promotions: state.promotions,
-    leaders: state.leaders
+    //leaders: state.leaders
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
   fetchDishes: () => { dispatch(fetchDishes())},
-  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
-
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos()),
+  //fetchLeaders: () => dispatch(fetchLeaders()),
 });
 
 
@@ -39,21 +41,38 @@ class Main extends Component {
 
     };
 
-  componentDidMount() {
-    this.props.fetchDishes();
-  }
+    componentDidMount() {
+      this.props.fetchDishes();
+      this.props.fetchComments();
+      this.props.fetchPromos();
+      //this.props.fetchLeaders();
+    }
 
   render() {
 
     const HomePage = () => {
       return(
-          <Home 
-              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-              dishesLoading={this.props.dishes.isLoading}
-              dishesErrMess={this.props.dishes.errMess}
-              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-          />
+        // <Home 
+        //   dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+        //   dishesLoading={this.props.dishes.isLoading}
+        //   dishesErrMess={this.props.dishes.errMess}
+        //   promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+        //   promoLoading={this.props.promotions.isLoading}
+        //   promoErrMess={this.props.promotions.errMess}
+        //   leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+        //   // leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+        //   // leaderLoading={this.props.leaders.isLoading}
+        //   // leaderErrMess={this.props.leaders.errMess}
+        // />
+        <Home 
+        dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+        dishesLoading={this.props.dishes.isLoading}
+        dishesErrMess={this.props.dishes.errMess}
+        promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+        promoLoading={this.props.promotions.isLoading}
+        promoErrMess={this.props.promotions.errMess}
+        //leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+    />
       );
     }
     //because <Route link that calls this function has path="/menu/:dishId", 
@@ -64,10 +83,11 @@ class Main extends Component {
           <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
             isLoading={this.props.dishes.isLoading}
             errMess={this.props.dishes.errMess}
-            //   //returns a comments array
+            //returns a comments array
             comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-            //   //allows addComment function to be used
+            //allows addComment function to be used
             addComment={this.props.addComment}
+            commentsErrMess={this.props.comments.errMess}
           />
       );
     };
